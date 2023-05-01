2a2ece7b3a1fa0426a3d2fa4b018adc0f280a009
Mark Flerko <markflerko@gmail.com>
2023-05-01 18:08:27 +0300

    feat(Router): init

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,63 @@
 import { AppShell } from '@mantine/core';
-import './App.css';
+import { Link, Outlet, Route, Routes } from 'react-router-dom';
 import { CustomHeader } from './components/Header';
 
-function App() {
+export default function App() {
   return (
-    <AppShell padding="md" header={CustomHeader()}>
-      {/* Your application here */}
-    </AppShell>
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="search" element={<Search />} />
+          <Route path="vacancy" element={<Vacancy />} />
+          <Route path="favorites" element={<Favorites />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
-export default App;
+function Layout() {
+  return (
+    <div>
+      <AppShell padding="md" header={CustomHeader()}>
+        <Outlet />
+      </AppShell>
+    </div>
+  );
+}
+
+function Search() {
+  return (
+    <div>
+      <h2>Search</h2>
+    </div>
+  );
+}
+
+function Vacancy() {
+  return (
+    <div>
+      <h2>Vacancy</h2>
+    </div>
+  );
+}
+
+function Favorites() {
+  return (
+    <div>
+      <h2>Favorites</h2>
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,27 @@
 import { Header, Text } from '@mantine/core';
+import { useNavigate } from 'react-router-dom';
 import styles from './Header.module.scss';
 import logo from './logo.svg';
 
 export const CustomHeader = () => {
+  const navigate = useNavigate();
+
+  const handleSearchClick = () => {
+    navigate('/search');
+  };
+
+  const handleFavoritesClick = () => {
+    navigate('/vacancy');
+  };
+
   return (
     <Header height={84} className={styles.CustomHeader}>
       <div className={styles.Logo}>
         <img src={logo} alt="Logo" />
       </div>
       <div className={styles.Menu}>
-        <Text>Поиск вакансий</Text>
-        <Text>Избранное</Text>
+        <Text onClick={handleSearchClick}>Поиск вакансий</Text>
+        <Text onClick={handleFavoritesClick}>Избранное</Text>
       </div>
     </Header>
   );
